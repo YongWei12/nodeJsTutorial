001359e30d55df49dd3b04a23a71d007b916e6a5
Yong Wei <yongwei996@gmail.com>
2020-06-07 16:37:52 +0800

    added the model for the project

--- app.js
+++ app.js
@@ -2,9 +2,9 @@
 const bodyParser = require('body-parser')
 const express = require('express');
 const path = require('path');
-
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
+const errorController = require('./controllers/error')
 
 //express exports as a function
 const app = express()
@@ -19,11 +19,9 @@ app.use(bodyParser.urlencoded({extended:false}));
 //static will make the folder public to outside user
 app.use(express.static(path.join(__dirname,"public")));
 
-app.use('/admin',adminRoutes.routes);
+app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 
-app.use((req,res,next)=>{
-    res.status(404).render('404',{pageTitle: 'Page Not Found'});
-});
+app.use(errorController.get404);
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -3,23 +3,11 @@ const rootDir = require('../util/path')
 
 const express = require('express');
 const router = express.Router();
-
 const adminData = require('./admin');
+const productsController = require('../controllers/products')
 
 
-router.get('/',(req, res, next)=>{
-    //path join will build path for both linux and windows
-    //__dirname is direct path to the source folder
-    const products = adminData.products;
-    res.render('shop',{
-        prods: products,
-        pageTitle: 'Shop',
-        path: '/',
-        hasProducts: products.length > 0,
-        activeShop: true,
-        productCSS: true
-    })
-});
+router.get('/', productsController.getProducts);
 
 
 module.exports = router;
