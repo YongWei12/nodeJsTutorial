2c055afda52f1b2ab82b46f460724bc30be8da14
Yong Wei <yongwei996@gmail.com>
2020-06-08 16:27:12 +0800

    added teh new express project folder

---
+++ after_enhancing app/routes/shop.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+const express = require('express');
+
+const shopController = require('../controllers/shop');
+
+const router = express.Router();
+
+router.get('/', shopController.getIndex);
+
+router.get('/products', shopController.getProducts);
+
+// : allow you to extract the variable, should add it as the last
+router.get('/products/:productId', shopController.getProduct);
+
+router.post('/cart', shopController.postCart);
+
+router.get('/cart', shopController.getCart);
+
+router.get('/orders', shopController.getOrders);
+
+router.get('/checkout', shopController.getCheckout);
+
+module.exports = router;
